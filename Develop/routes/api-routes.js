237947e4744bbb26706f5a3e75c7ef4e0cfc19cb
Joshua Hale <joshuahale829@gmail.com>
2024-02-12 10:21:10 -0600

    Wrote the api_routes.js file.

---
+++ Develop/routes/api-routes.js
@@ -0,0 +1,36 @@
+// Purpose: This file contains the routes for the api.
+const router = require("express").Router();
+const { v4: uuidv4 } = require("uuid").v4;
+const fs = require("fs");
+
+// Route to index.html
+router.get('/api/notes', async (req, res) => {
+    const dbJson = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
+    res.json(dbJson);
+});
+
+// Route to notes.html
+router.post('/api/notes', async (req, res) => {
+    const dbJson = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
+    const newFeedback = {
+        title: req.body.title,
+        text: req.body.text,
+        id: uuidv4()
+    };
+    dbJson.push(newFeedback);
+    fs.writeFileSync('./db/db.json', JSON.stringify(dbJson));
+    res.json(dbJson);
+});
+
+// Route for delete request
+router.delete('/api/notes/:id', (req, res) => {
+    let data = fs.readFileSync("db/db.json", "utf8");
+    const dataJSON =  JSON.parse(data);
+    const newNotes = dataJSON.filter((note) => { 
+      return note.id !== req.params.id;
+    });
+    fs.writeFileSync("db/db.json",JSON.stringify(newNotes));
+    res.json("Note deleted.");
+  });
+
+module.exports = router;
