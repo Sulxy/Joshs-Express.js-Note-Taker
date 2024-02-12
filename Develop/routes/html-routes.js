d8198cef5c3dcfdce3cfd18ab3c97a7176fb8d21
Joshua Hale <joshuahale829@gmail.com>
2024-02-12 09:50:13 -0600

    Wrote html_routes.js file, and commented it.

---
+++ Develop/routes/html-routes.js
@@ -0,0 +1,16 @@
+// Purpose: This file is used to route the user to the correct html page when the user navigates to the site.
+const router = require("express").Router();
+const path = require("path");
+
+// Routes to index.html
+router.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
+// Routes to notes.html
+router.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/notes.html'));
+});
+
+// Export the router
+module.exports = router;
